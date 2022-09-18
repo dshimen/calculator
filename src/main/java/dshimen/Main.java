acdc55b3bb99e6ca327ddced8c936f2e8880fef0
dshimen <blach7654321@gmail.com>
2022-09-19 00:01:56 +0200

    almost working

--- src/main/java/dshimen/Main.java
+++ src/main/java/dshimen/Main.java
@@ -3,11 +3,11 @@ package dshimen;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
 
 public class Main {
-    //(aa)*bb(a|b)(a)?
     public static void main(String[] args) throws IOException {
-        System.out.println("Program sprawdza czy dany ciąg liter AB spełnia wyrażenie {aa}bb(a|b)[a]");
+        System.out.println("Program jest prostym kalkulatorem");
         String dataString = "";
         while(!dataString.equals("quit") && !dataString.equals("q")){
             System.out.print("Wpisz ciąg znaków składający się tylko z ()[0-9]+-*/: ");
@@ -16,9 +16,311 @@ public class Main {
             System.out.println("Wynik zadanego wyrazenia to: " + calculate(dataString));
         }
     }
+    //7*3*(4*3)*(((30-3*15)-20)-15)/7+14/7+3/2
     public static int calculate(String dataString) {
-        Parser.parser(dataString);
-        int state = 0;
+        List<Object> elements = Parser.parser(dataString);
+        if(elements == null){
+            System.out.println("Wyrażenie było błędne i dało się go policzyć");
+            System.exit(0);
+        }else{
+            int state = 0; //1 - plus, 2 - minus, 3 - multiply, 4 - divide
+            double currentCalc = 0;
+            removeMultiply(elements);
+            removeDevide(elements);
+            simplifyBrackets(elements);
+            removeSomeBrackets(elements);
+            StringBuilder orginal = new StringBuilder();
+            StringBuilder toCompare_2;
+            for(Object o : elements){
+                orginal.append(o);
+            }
+            while (true){
+                toCompare_2 = new StringBuilder();
+                removeMultiply(elements);
+                removeDevide(elements);
+                simplifyBrackets(elements);
+                removeSomeBrackets(elements);
+                for(Object o : elements){
+                    toCompare_2.append(o);
+                }
+                System.out.println("orginal: " + orginal);
+                System.out.println("tocomapre: " + toCompare_2);
+                if(!orginal.toString().equals(toCompare_2.toString())){
+                    orginal = toCompare_2;
+                }else {
+                    break;
+                }
+            }
+//            while (elements.size() > 3){
+//                finalSimplify(elements);
+//            }
+            for(Object o : elements){
+                System.out.print(o + " ");
+            }
+            System.out.println("");
+            System.out.println(currentCalc);
+        }
         return 0;
+    }
+
+    private static List<Object> removeMultiply(List<Object> elements){
+        boolean beforeAllMultuply = true;
+        while (beforeAllMultuply){
+            for(int i = 0;i < elements.size();i ++){
+                if(elements.get(i) instanceof Integer){
+                    if(i > 1){
+                        if(elements.get(i-2) instanceof Integer){
+                            if((char)elements.get(i-1) == '*'){
+                                elements.set(i, (int) elements.get(i) * (int) elements.get(i-2));
+                                elements.remove(i-1);
+                                elements.remove(i-2);
+                                break;
+                            }
+                        } else if(elements.get(i-2) instanceof Double){
+                            if((char)elements.get(i-1) == '*'){
+                                elements.set(i, (int) elements.get(i) * (double) elements.get(i-2));
+                                elements.remove(i-1);
+                                elements.remove(i-2);
+                                break;
+                            }
+                        }
+                    }
+                }else if(elements.get(i) instanceof Double){
+                    if(i > 1){
+                        if(elements.get(i-2) instanceof Double){
+                            if((char)elements.get(i-1) == '*'){
+                                elements.set(i, (double) elements.get(i) * (double) elements.get(i-2));
+                                elements.remove(i-1);
+                                elements.remove(i-2);
+                                break;
+                            }
+                        }else if(elements.get(i-2) instanceof Integer){
+                            if((char)elements.get(i-1) == '*'){
+                                elements.set(i, (double) elements.get(i) * (int) elements.get(i-2));
+                                elements.remove(i-1);
+                                elements.remove(i-2);
+                                break;
+                            }
+                        }
+                    }
+                }
+                if( i == elements.size()-1){
+                    beforeAllMultuply = false;
+                }
+            }
+        }
+        return elements;
+    }
+    private static List<Object> removeDevide(List<Object> elements){
+        boolean beforeAllMultuply = true;
+        while (beforeAllMultuply){
+            for(int i = 0;i < elements.size();i ++){
+                if(elements.get(i) instanceof Integer){
+                    if(i > 1){
+                        if(elements.get(i-2) instanceof Integer){
+                            if((char)elements.get(i-1) == '/'){
+                                elements.set(i, ((int) elements.get(i - 2)) * 1.0 / ((int) elements.get(i)));
+                                elements.remove(i-1);
+                                elements.remove(i-2);
+                                break;
+                            }
+                        }else if(elements.get(i-2) instanceof Double){
+                            if((char)elements.get(i-1) == '/'){
+                                elements.set(i, ((double) elements.get(i - 2)) / ((int) elements.get(i)));
+                                elements.remove(i-1);
+                                elements.remove(i-2);
+                                break;
+                            }
+                        }
+                    }
+                } else if(elements.get(i) instanceof Double){
+                    if(i > 1){
+                        if(elements.get(i-2) instanceof Double){
+                            if((char)elements.get(i-1) == '/'){
+                                elements.set(i, ((double) elements.get(i - 2)) / ((double) elements.get(i)));
+                                elements.remove(i-1);
+                                elements.remove(i-2);
+                                break;
+                            }
+                        } else if(elements.get(i-2) instanceof Integer){
+                            if((char)elements.get(i-1) == '/'){
+                                elements.set(i, ((int) elements.get(i - 2)) * 1.0 / ((double) elements.get(i)));
+                                elements.remove(i-1);
+                                elements.remove(i-2);
+                                break;
+                            }
+                        }
+                    }
+                }
+                if( i == elements.size()-1){
+                    beforeAllMultuply = false;
+                }
+            }
+        }
+        return elements;
+    }
+
+    private static List<Object> simplifyBrackets(List<Object> elements){
+        boolean beforeAllMultuply = true;
+        while (beforeAllMultuply){
+            for(int i = 0;i < elements.size();i ++){
+                if(elements.get(i) instanceof Integer){
+                    if(i > 2){
+                        if(elements.get(i-2) instanceof Integer){
+                            if((char)elements.get(i-1) == '-' && (char)elements.get(i-3) == '('){
+                                elements.set(i, ((int) elements.get(i - 2)) * 1.0 - ((int) elements.get(i)));
+                                elements.remove(i-1);
+                                elements.remove(i-2);
+                                break;
+                            }else if((char)elements.get(i-1) == '+' && (char)elements.get(i-3) == '('){
+                                elements.set(i, ((int) elements.get(i - 2)) * 1.0 + ((int) elements.get(i)));
+                                elements.remove(i-1);
+                                elements.remove(i-2);
+                                break;
+                            }
+                        }
+                        if(elements.get(i-2) instanceof Double){
+                            if((char)elements.get(i-1) == '-' && (char)elements.get(i-3) == '('){
+                                elements.set(i, ((double) elements.get(i - 2)) - ((int) elements.get(i))*1.0);
+                                elements.remove(i-1);
+                                elements.remove(i-2);
+                                break;
+                            }else if((char)elements.get(i-1) == '+' && (char)elements.get(i-3) == '('){
+                                elements.set(i, ((double) elements.get(i - 2)) + ((int) elements.get(i))*1.0);
+                                elements.remove(i-1);
+                                elements.remove(i-2);
+                                break;
+                            }
+                        }
+                    }
+                }else if(elements.get(i) instanceof Double){
+                    if(i > 2){
+                        if(elements.get(i-2) instanceof Double){
+                            if((char)elements.get(i-1) == '-' && (char)elements.get(i-3) == '('){
+                                elements.set(i, ((double) elements.get(i - 2)) - ((double) elements.get(i)));
+                                elements.remove(i-1);
+                                elements.remove(i-2);
+                                break;
+                            }else if((char)elements.get(i-1) == '+' && (char)elements.get(i-3) == '('){
+                                elements.set(i, ((double) elements.get(i - 2)) + ((double) elements.get(i)));
+                                elements.remove(i-1);
+                                elements.remove(i-2);
+                                break;
+                            }
+                        } else if(elements.get(i-2) instanceof Integer){
+                            if((char)elements.get(i-1) == '-' && (char)elements.get(i-3) == '('){
+                                elements.set(i, ((int) elements.get(i - 2))*1.0 - ((double) elements.get(i)));
+                                elements.remove(i-1);
+                                elements.remove(i-2);
+                                break;
+                            }else if((char)elements.get(i-1) == '+' && (char)elements.get(i-3) == '('){
+                                elements.set(i, ((int) elements.get(i - 2))*1.0 + ((double) elements.get(i)));
+                                elements.remove(i-1);
+                                elements.remove(i-2);
+                                break;
+                            }
+                        }
+                    }
+                }
+                if( i == elements.size()-1){
+                    beforeAllMultuply = false;
+                }
+            }
+        }
+        return elements;
+    }
+    private static List<Object> removeSomeBrackets(List<Object> elements){
+        boolean beforeAllMultuply = true;
+        while (beforeAllMultuply){
+            for(int i = 0;i < elements.size();i ++){
+                if(i > 1){
+                    if(elements.get(i) instanceof Integer){
+                        if(elements.get(i-2) instanceof Integer){
+                            if((char)elements.get(i-1) == '-'){
+                                elements.set(i, ((int) elements.get(i - 2)) * 1.0 - ((int) elements.get(i)));
+                                elements.remove(i-1);
+                                elements.remove(i-2);
+                                break;
+                            }else if((char)elements.get(i-1) == '+'){
+                                elements.set(i, ((int) elements.get(i - 2)) * 1.0 + ((int) elements.get(i)));
+                                elements.remove(i-1);
+                                elements.remove(i-2);
+                                break;
+                            }
+                        }else if(elements.get(i-2) instanceof Double){
+                            if((char)elements.get(i-1) == '-'){
+                                elements.set(i, ((double) elements.get(i - 2)) - ((int) elements.get(i)));
+                                elements.remove(i-1);
+                                elements.remove(i-2);
+                                break;
+                            }else if((char)elements.get(i-1) == '+'){
+                                elements.set(i, ((double) elements.get(i - 2)) + ((int) elements.get(i)));
+                                elements.remove(i-1);
+                                elements.remove(i-2);
+                                break;
+                            }
+                        }
+
+                    }else if(elements.get(i) instanceof Double){
+                        if(elements.get(i-2) instanceof Integer){
+                            if((char)elements.get(i-1) == '-'){
+                                elements.set(i, ((int) elements.get(i - 2)) * 1.0 - ((double) elements.get(i)));
+                                elements.remove(i-1);
+                                elements.remove(i-2);
+                                break;
+                            }else if((char)elements.get(i-1) == '+'){
+                                elements.set(i, ((int) elements.get(i - 2)) * 1.0 + ((double) elements.get(i)));
+                                elements.remove(i-1);
+                                elements.remove(i-2);
+                                break;
+                            }
+                        }else if(elements.get(i-2) instanceof Double){
+                            if((char)elements.get(i-1) == '-'){
+                                elements.set(i, ((double) elements.get(i - 2)) - ((double) elements.get(i)));
+                                elements.remove(i-1);
+                                elements.remove(i-2);
+                                break;
+                            }else if((char)elements.get(i-1) == '+'){
+                                elements.set(i, ((double) elements.get(i - 2)) + ((double) elements.get(i)));
+                                elements.remove(i-1);
+                                elements.remove(i-2);
+                                break;
+                            }
+                        }
+                    }
+
+                }
+                if( i == elements.size()-1){
+                    beforeAllMultuply = false;
+                }
+            }
+        }
+        return elements;
+    }
+    private static List<Object> finalSimplify(List<Object> elements){
+        boolean beforeAllMultuply = true;
+        while (beforeAllMultuply){
+            for(int i = 0;i < elements.size();i ++){
+                if(i > 1){
+                    if(elements.get(i-1) instanceof Integer){
+                        if((char)elements.get(i-2) == '(' && (char)elements.get(i) == ')'){
+                            elements.remove(i);
+                            elements.remove(i-2);
+                            break;
+                        }
+                    }else if(elements.get(i-1) instanceof Double){
+                        if((char)elements.get(i-2) == '(' && (char)elements.get(i) == ')'){
+                            elements.remove(i);
+                            elements.remove(i-2);
+                            break;
+                        }
+                    }
+                }
+                if( i == elements.size()-1){
+                    beforeAllMultuply = false;
+                }
+            }
+        }
+        return elements;
     }
 }
--- src/main/java/dshimen/Parser.java
+++ src/main/java/dshimen/Parser.java
@@ -4,50 +4,86 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Parser {
-    public static void parser(String dataString) {
+    public static List<Object> parser(String dataString) {
         char[] dataChar = dataString.replace(" ","").toCharArray();
-        List<Inside> insideBrackets = new ArrayList<>();
-        int putInside = -1;
-        int state = 0;  //66 - state was number
+        List<Object> elements = new ArrayList<>();
+        int state = 0;  //66 - state was number, 1 - plus, 2 - minus, 3 - multiply, 3 - divide
         for(int i = 0;i < dataChar.length;i++){
             if(dataChar[i] == '('){
-                putInside++;
                 if(state == 66){
                     state = 0;
+                    elements.add(getNumber(dataChar, dataString, i));
+                    elements.add('(');
+                }else {
+                    elements.add('(');
                 }
             }else if(dataChar[i] == ')'){
-                putInside--;
                 if(state == 66){
                     state = 0;
+                    elements.add(getNumber(dataChar, dataString, i));
+                    elements.add(')');
+                }else {
+                    elements.add(')');
                 }
             }else if(dataChar[i] == '+'){
                 if(state == 66){
-                    state = 0;
+                    state = 1;
+                    elements.add(getNumber(dataChar, dataString, i));
+                    elements.add('+');
+                }else {
+                    elements.add('+');
                 }
             }else if(dataChar[i] == '-'){
                 if(state == 66){
-                    state = 0;
+                    state = 2;
+                    elements.add(getNumber(dataChar, dataString, i));
+                    elements.add('-');
+                }else {
+                    elements.add('-');
                 }
             }else if(dataChar[i] == '*'){
                 if(state == 66){
-                    state = 0;
+                    state = 3;
+                    elements.add(getNumber(dataChar, dataString, i));
+                    elements.add('*');
+                }else {
+                    elements.add('*');
                 }
             }else if(dataChar[i] == '/'){
                 if(state == 66){
-                    state = 0;
+                    state = 4;
+                    elements.add(getNumber(dataChar, dataString, i));
+                    elements.add('/');
+                }else {
+                    elements.add('/');
                 }
             }else if(isNumber(dataChar[i])){
                 state = 66;
+            }else{
+                return null;
             }
         }
         if(state == 66){
-            for(int i = dataChar.length-1; i >= 0;i--){
-                if(!isNumber(dataChar[i])){
-                    System.out.println("number: " + Integer.parseInt(dataString.substring(i+1, dataChar.length)));
-                    break;
+            elements.add(getNumber(dataChar, dataString, dataChar.length));
+        }
+        return elements;
+    }
+    private static int getNumber(char [] dataChar, String dataString, int length){
+        for(int i = length-1; i >= 0;i--){
+            if(!isNumber(dataChar[i]) || i == 0){
+                if(dataChar[i] == '-' && ((i > 0 && dataChar[i-1] == '-') || (i == 0))){
+                    return  Integer.parseInt(dataString.substring(i, length));
+                }else{
+                    if(i==0){
+                        return  Integer.parseInt(dataString.substring(i, length));
+                    }else{
+                        return  Integer.parseInt(dataString.substring(i+1, length));
+                    }
                 }
             }
         }
+        System.out.println("ERROR");
+        return 0;
     }
     private static boolean isNumber(char number){
         if(number >= 48 && number <= 57){
